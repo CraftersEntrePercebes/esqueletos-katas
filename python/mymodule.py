a35f998d705581f4e723db37d91a09731199b025
Isidro López <islomar@gmail.com>
2018-01-04 13:49:04 +0100

    Añadido esqueleto para Python

---
+++ python/mymodule.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def my_function():
+    pass
---
+++ python/mymodule_spec.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from expects import *
+
+
+with describe('Fizzbuzz'):
+    with context('when xx'):
+        with it('yy'):           
+            expect(True).to(be_true)
+            
