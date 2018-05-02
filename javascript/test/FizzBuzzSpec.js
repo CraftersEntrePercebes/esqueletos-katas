833020f483912692505b9088afda71aa3ccddbe4
Isidro López <islomar@gmail.com>
2018-05-02 21:14:31 +0200

    [javascript] Include basic structure for first test

--- javascript/test/FizzBuzzSpec.js
+++ javascript/test/FizzBuzzSpec.js
@@ -1,13 +1,18 @@
 var expect = require('chai').expect;
 
+function fizzbuzz(number) {
+  //TODO
+}
+
 describe('FizzBuzz()', function() {
 
   it('should xxxx', function() {
     //Arrange
 
     //Act
+    let result = fizzbuzz(1)
 
     //Assert
-
+    expect(result).to.equal("1")
   });
 });
